69a75a84dc763541276c2b5a8a07dfc2928f60d2
razib.hasan <razib.hasan@tuni.fi>
2023-01-30 01:13:18 +0200

    first commit

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to calculator!");
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Enter the operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + result);
+                break;
+            case '/':
+                result = num1 / num2;
+                System.out.println(num1 + " / " + num2 + " = " + result);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+    }
+}
